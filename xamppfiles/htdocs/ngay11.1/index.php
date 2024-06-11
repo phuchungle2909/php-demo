48226ca480e8692fa5d714a76eb6ab40942bcc43
Út Bình <nguyenhung@mbp.local>
2024-06-11 21:51:18 +0700

    xoa xuong dong

--- xamppfiles/htdocs/ngay11.1/index.php
+++ xamppfiles/htdocs/ngay11.1/index.php
@@ -6,13 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
-
-
-
-
-
-
 <body>
     <!-- Tiêu đề trang -->
     <h3>Trang web user (Trang danh cho người dùng sử dụng)</h3>
